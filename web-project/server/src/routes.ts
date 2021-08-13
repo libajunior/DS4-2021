e88d533fbd0bd5a59fe97904335c2d7027bac3aa
libajr <libajr@materdei.edu.br>
2021-08-12 23:30:17 -0300

    Aula 12/08/2021

--- web-project/server/src/routes.ts
+++ web-project/server/src/routes.ts
@@ -4,9 +4,8 @@ import AuthController from './controllers/AuthController';
 const routes = Router();
 
 //AUTH
-routes.use('/auth')
-    .post('/signup', AuthController.create)
-    .post('signin', AuthController.validate);
+routes.post('/auth/signup', AuthController.create);
+routes.post('/auth/signin', AuthController.validate);
 
 
 
